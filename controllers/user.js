d38db7e5a5151f7a277c1fd382e425134bcdd7f8
Thomas Gilbert <tcgilbert94@gmail.com>
2021-01-20 15:04:03 -0500

    [user controller] signup correctly hashing password

--- controllers/user.js
+++ controllers/user.js
@@ -17,4 +17,42 @@ router.get('/test', (req, res) => {
     res.json({msg: 'you have hit the users route'})
 })
 
+// sign up route
+router.post('/signup', async (req, res) => {
+    console.log('hit the signup route');
+    try {
+        const requestedUser = await db.user.findOne({
+            where: { email: req.body.email }
+        })
+        // check for email
+        if (requestedUser) {
+            return res.status(400).json({ msg: 'Email already in use' })
+        } else {
+            // create new user
+            const newUser = await db.user.build({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            })
+            // create salt for password
+            bcrypt.genSalt(10, (error, salt) => {
+                if (error) throw Error
+                bcrypt.hash(newUser.password, salt, async (error, hash) => {
+                    try {
+                        if (error) throw Error
+                        // else hash password
+                        newUser.password = hash
+                        const createdUser = await newUser.save()
+                        res.status(201).json(createdUser)
+                    } catch (error) {
+                        console.log(`HASHING ERROR: ${error}`);
+                    }
+                })
+            })
+        } 
+    } catch(err) {
+        console.log(`SIGNUP ERROR: ${err}`);
+    }
+})
+
 module.exports = router
---
+++ dbTester.js
@@ -0,0 +1,13 @@
+const db = require('./models')
+
+// db.user.create({
+//     email: "blah@aol.com",
+//     name: "Thomas"
+// }).then((res) => {
+//     console.log(res);
+// })
+
+// db.user.findOne({ where: {email: 'blah@aol.com'} })
+// .then((res) => {
+//     console.log(res);
+// })
